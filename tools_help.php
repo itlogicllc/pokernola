bc1542302cfa9d9e75815301c606c506e383fab5
Keith <Keith@ITLOGIC-PC.no.cox.net>
2015-01-25 12:43:42 -0600

    Removed version information

--- tools_help.php
+++ tools_help.php
@@ -8,11 +8,10 @@
    <body>
       <div data-role="page" id="pokernola_help">
          <div data-role="header" data-position="fixed">
-            <h1>Poker NOLA Help</h1>
+            <h1>Help</h1>
             <?php require('includes/set_tools.php'); ?>
          </div>
          <div role="main" class="ui-content">
-            <div class="ui-bar ui-bar-a ui-corner-all" align="center"><h2>Poker NOLA v1.1.0</h2></div><br />
             <div class="ui-corner-all custom-corners">
                <div class="ui-bar ui-bar-a">
                   <h2><img src="jquery-mobile/images/icons-png/info-white.png" /> Overview</h2>
